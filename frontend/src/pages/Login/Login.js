2d539780bc867523af37b04c212b7b189b4c5884
Marcus <39126245+marcusohx@users.noreply.github.com>
2023-09-11 00:22:27 +0800

    Update Login.js

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -2,22 +2,32 @@ import React, { useState, useContext } from "react";
 import axios from "axios";
 import "./Login.css";
 import { AuthContext } from "../../contexts/AuthContext"; // Adjust the path as necessary
+import { useNavigate } from "react-router-dom"; // Import useHistory
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const { login } = useContext(AuthContext);
+  const navigate = useNavigate(); // Instantiate useHistory
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post("http://localhost:3001/user/login", {
-        email,
-        password,
-      });
+      const response = await axios.post(
+        "http://localhost:3001/user/login",
+        {
+          email,
+          password,
+        },
+        {
+          withCredentials: true, // Ensure credentials are included
+        }
+      );
       console.log(response.data);
 
-      login({ email, password }); // call the login function from AuthContext
+      await login({ email, password }); // call the login function from AuthContext
+
+      navigate("/"); // Redirect to the home page
     } catch (error) {
       console.log(error);
     }
