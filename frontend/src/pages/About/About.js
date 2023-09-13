8f45e5822011685d926ac994c6294959a2477ee4
sammtann <sammtannzxc@gmail.com>
2023-09-13 16:25:00 +0800

    About page (base)

---
+++ frontend/src/pages/About/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Welcome to the about page!</p>
+    </div>
+  );
+}
+
+export default About;
